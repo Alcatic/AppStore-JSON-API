e3b2ea76fe401cdd5694b054a674add2459acf17
Chad-Michael Muirhead <chadmuirhead@live.com>
2019-07-16 21:36:51 -0400

    Basic URLSession Request

---
+++ AppStore JSON API/Search/SearchCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchCell.swift
+//  AppStore JSON API
+//
+//  Created by Di_Nerd on 7/16/19.
+//  Copyright © 2019 Di_Nerd. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+    
+    @IBOutlet weak var appImage: UIImageView!
+    
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var appSubtitle: UILabel!
+    @IBOutlet weak var appRating: UILabel!
+    
+    
+    @IBOutlet weak var firstPreviewImage: UIImageView!
+    
+    @IBOutlet weak var secondPreviewImage: UIImageView!
+    
+    @IBOutlet weak var thirdPreviewImage: UIImageView!
+    
+    
+}
