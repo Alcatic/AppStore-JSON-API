7839ec522c0d31da9f7aca2df2fb990c244b4a3a
Chad-Michael Muirhead <chadmuirhead@live.com>
2019-07-16 20:55:43 -0400

    Initial Commit

---
+++ AppStore JSON API/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AppStore JSON API
+//
+//  Created by Di_Nerd on 7/16/19.
+//  Copyright © 2019 Di_Nerd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
