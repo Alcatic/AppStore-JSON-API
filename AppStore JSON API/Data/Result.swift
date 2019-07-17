71297993d4d5f9e93982ab6daca263fca4dfa2c5
Chad-Michael Muirhead <chadmuirhead@live.com>
2019-07-16 23:12:55 -0400

    SDWebImage added with Pods

--- AppStore JSON API/Data/Result.swift
+++ AppStore JSON API/Data/Result.swift
@@ -12,4 +12,11 @@ struct Result: Decodable{
     let trackName: String
     let primaryGenreName: String
     let averageUserRating: Float?
+    let screenshotUrls: [String]
+    let artworkUrl100: String //app icon
+}
+
+struct SearchResult: Decodable{
+    let resultCount: Int
+    let results: [Result]
 }
